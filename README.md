a73db97170ca7bd3f04461d92ed3bef6e1a9bd97
Jesse Monroy <jessemonroy650@yahoo.com>
2015-07-06 02:31:28 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# jscurlys
+# jscurlys #
 an HTML template engine for static pages, similar to mustache.js or handlebars.js
+
+## Basic Operation ##
+Date: 2015-07-05 (1436134788)
+
+***EXAMPLE Usage***
+> cat filename.curly | ./jscurlys.tcl > filename.html
+
+Inside your (HTML/any text) file you have:
+```
+some string with a {{name}} that will be substituted
+```
+
+There are two types of substitutions:
+
+* name value
+* name File:[/FQFN/|RQFN/]filename
+
+```
+name a value string
+name File:filename
+```
+
+
+## Technical Details ##
+JsCurly a simple HTML template engine, written in tclsh.
+
+### Environment Variables ###
+
+The Environment Variables are the filenames of text files.
+If no values given, then *jscurlys* uses the default filenames.
+The default filenames must be in the current directory.
+
+* CURLYGLOBAL - default-filename:curlyGLOBALS
+* CURLYDATA   - default-filename:curlyData
+
+### CURLYGLOBAL file ###
+
+These MUST BE defined. There are only three (3) GLOBALS defined.
+These three (3) "curlys" should be in your HTML file.
+
+If you do NOT want use them in your HTML file, don't use them,
+but they must be defined in your *CURLYGLOBAL* file.
+
+**"curly" that should be in the *HTML* file*
+
+* {{META}}
+* {{TITLE}}
+* {{CSSLOCAL}}
+
+**In the *CURLYGLOBAL* file** (example)
+
+```
+META File:curlyfiles/meta.xml
+TITLE The tile for the file
+CSSLOCAL File:curlyfiles/csslocal.css
+```
+
+*example of an unused*
+
+```
+META .
+TITLE .
+CSSLOCAL .
+```
+
+### CURLYGLOBAL pre-defined ###
+
+These are temporal values. The values 
+
+curlys | internal format | example output
+-----------------------------------------
+{{DATE}}      | %e-%b-%Y | 6-Jul-2015
+{{DATE2}}     | %D       | 07/06/2015
+{{YYYYMMDD}}  | %Y-%m-%d | 2015-07-06
+{{TIME}}      | %T       | 01:20:25
+{{EPOCH}}     | %s       | 1436170825
+{{YEAR}}      | %Y       | 2015
+{{GENERATOR}} | version of 'jscurlys' | JsCurlys v0.9.5
+
+### CURLYDATA file ###
+
+I think most people will get the name/value pair idea. 
+I also think the get the name/File:filename idea.
+
+The name will translate internally to a variable name.
+This means the *name* is limited to (A-Z),(a-z),(0-9),(_).
+*name* with all capitals is reserved for GLOBAL internals.
+
+**Examples**
+
+```
+Title This is a title.
+Ads File:fileWithAds.js
+headerImageLeft images/3menubars_black.png
+```
+
---
+++ jscurlys.tcl
@@ -0,0 +1,214 @@
+#!/bin/sh
+#
+#	jscurly.tcl
+#	Date: 2015-07-05
+#	Jesse Monroy, Jr. (jessemonroy650@yahoo.com)
+#
+#----------------------------\
+exec tclsh $0 "$@"
+
+set DEBUG_DEBUG   0
+#----------------------------
+set	GENERATOR   "JsCurlys v0.9.5"
+#----------------------------
+
+set	Home        ""
+set CurlyGlobal ""
+set CurlyData   ""
+# Check for Environment Variables
+if { [info exists env(HOME) ] } {
+	set	Home        $::env(HOME)
+}
+if { [info exists ::env(CURLYGLOBAL) ] } {
+	set CurlyGlobal $::env(CURLYGLOBAL)
+}
+if { [info exists ::env(CURLYDATA) ] } {
+	set CurlyData   $::env(CURLYDATA)
+}
+# Set to defaults, if we got no Envirnoment Varibles
+if { $CurlyGlobal == "" } { set CurlyGlobal "curlyGLOBALS" }
+if { $CurlyData   == "" } { set CurlyData   "curlyData" }
+
+#puts [join "CurlyGlobal:\"$CurlyGlobal\"" ]
+#puts [join "CurlyData:\"$CurlyData\"" ]
+#exit
+
+#============================================#
+#	Static Variables
+#============================================#
+# file of Global Document Meta Variables
+set	CurlyGlobal		$CurlyGlobal
+
+# See Names: http://javascript.crockford.com/code.html
+array set marker {
+	meta	  {\{\{META\}\}}
+	title	  {\{\{TITLE\}\}}
+	csslocal  {\{\{CSSLOCAL\}\}}
+
+	date	{\{\{DATE\}\}}
+	date2	{\{\{DATE2\}\}}
+	date3	{\{\{YYYYMMDD\}\}}
+	time	{\{\{TIME\}\}}
+	epoch	{\{\{EPOCH\}\}}
+	year	{\{\{YEAR\}\}}
+	generator {\{\{GENERATOR\}\}}
+}
+
+#============================================#
+#	Temporal Variables
+#============================================#
+set	DATE	[clock format [clock seconds] -format "%e-%b-%Y"]
+set	DATE2	[clock format [clock seconds] -format "%D"]
+set	DATE3	[clock format [clock seconds] -format "%Y-%m-%d"]
+set	TIME	[clock format [clock seconds] -format %T]
+set	EPOCH	[clock format [clock seconds] -format %s]
+set	YEAR	[clock format [clock seconds] -format %Y]
+
+#============================================#
+#	Libraries
+#============================================#
+#source	./cvarload.tcl
+#source	./chashload.tcl
+#source	./cmvarload.tcl
+#source	$Lib/stack.tcl;		# As of 2015-02-04
+
+#@	Loads Variable from a file. That is, the entire file is the variable.
+proc commonVarFile { var filename } {
+
+	upvar $var lvar
+	set fileId [open $filename "r"]
+	set lvar [ read $fileId ]
+	close $fileId
+}
+#@	Loads self defined variables from a file. 
+#@	That is, every line is a 'variable name', followed by the 'variable value'.
+#
+proc commonMultipleVariableFile { filename } {
+
+	set fileId [open $filename "r"]
+	while { [gets $fileId ldata] >=0  } {
+		regexp {([^\s]+)\s+(.+)} $ldata theWholeMatch hkey hdata
+		#puts "$hkey $hdata"
+		uplevel #0 set $hkey \"$hdata\"
+	}
+	close $fileId
+}
+#@	Loads hash from a file. 
+#@	That is, every line is a 'hash key', followed by the 'hash data'.
+#
+proc commonHashFile { filename } {
+	global theHash
+
+	set fileId [open $filename "r"]
+	while { [gets $fileId ldata] >=0  } {
+		regexp {([^\s]+)\s+(.+)} $ldata theWholeMatch hkey hdata
+		#puts "$hkey $hdata"
+		array set theHash [ list $hkey $hdata ]
+	}
+	close $fileId
+}
+
+array set theHash {}
+
+#============================================#
+#	Instance Variables
+#============================================#
+# load (global) Document Meta Variables
+if [ file exists $CurlyGlobal ] {
+	commonHashFile $CurlyGlobal
+
+	set TITLE     $theHash(TITLE)
+	set META      $theHash(META)
+	set CSSLOCAL  $theHash(CSSLOCAL)
+
+	#
+	# Search all fields to see if the string indicates to use a file.
+	#
+	foreach vv [ array names theHash ] { 
+		if [ regexp {^(\S+):(.+)} $theHash($vv) theWholeMatch type parm ] {
+			if [ expr {{File} eq "$type"} ] {
+				commonVarFile tempvar $parm
+				set $vv $tempvar
+			}
+		}
+	}
+}
+
+
+# Load user data
+commonMultipleVariableFile $CurlyData
+
+#puts $TITLE
+#puts $META
+#puts $CSSLOCAL
+#puts $AppName
+#puts $CopyrightHolder
+#exit
+
+set	lineno	0
+set	oflag	0
+
+#============================================#
+#		M  A  I  N
+#============================================#
+while { [gets stdin line ] >= 0 } {
+	incr lineno
+	# 
+	if [ regexp {\{\{(File:([^\{]+))\}\}} $line theWholeMatch inCurly theFile ] {
+		commonVarFile substitute $theFile
+		# Double check substitution
+		if { $DEBUG_DEBUG == 1 } {
+			puts $inCurly
+	        puts $substitute
+            puts "theFile:$theFile"
+		}
+		# Do the substitution
+		regsub {\{\{([^\{]+)\}\}} $line $substitute line
+		# clear out temporay variables
+		set theWholeMatch {}
+		set inCurly {}
+		set theFile {}
+		set substitute {}
+    } elseif [ regexp {\{\{([^\{]+)\}\}} $line theWholeMatch inCurly ] {
+        eval set substitute $$inCurly
+		# Double check substitution
+		if { $DEBUG_DEBUG == 1 } {
+			puts $inCurly
+	        puts $substitute
+		}
+		# Do the substitution
+		regsub {\{\{([^\{]+)\}\}} $line $substitute line
+		# clear out temporay variables
+		set theWholeMatch {}
+		set inCurly {}
+		set substitute {}
+	}
+
+	if [ info exists TITLE ]     { regsub -all $marker(title) $line $TITLE line }
+	if [ info exists META ]      { regsub -all $marker(meta)  $line $META  line }
+	if [ info exists CSSlOCAL ]  { regsub -all $marker(csslocal) $line $CSSlOCAL line }
+
+	# These are all global and defined a run-time.
+	# moved to last so other text strings can use "date", "time" & "epoch"
+	regsub -all $marker(date)    $line $DATE  line
+	regsub -all $marker(date2)   $line $DATE2 line
+	regsub -all $marker(date3)   $line $DATE3 line
+	regsub -all $marker(time)    $line $TIME  line
+	regsub -all $marker(epoch)   $line $EPOCH line
+	regsub -all $marker(year)    $line $YEAR  line
+	regsub -all $marker(generator) $line $GENERATOR line
+
+	# append line to list
+	lappend theBody $line
+}
+
+#============================================#
+#		
+#============================================#
+set body [join $theBody "\n"]
+if { $DEBUG_DEBUG == 1} {
+	puts "\n----"
+}
+puts $body
+
+exit
